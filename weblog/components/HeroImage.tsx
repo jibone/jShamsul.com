9a31c41b5107963f1db002774b2e06b078aac02f
J Shamsul Bahri (jibone)) <jibone@gmail.com>
2022-02-09 00:34:56 +0800

    fix hero image

--- weblog/components/HeroImage.tsx
+++ weblog/components/HeroImage.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
+import IndexImg from "../public/assets/covers/index.jpg";
+import AboutImg from "../public/assets/covers/about.jpg";
 
 const HeroImage: React.FC<{ name: string }> = ({ name }) => {
-  const path = `/assets/covers/${name}`;
+  // const path = `/assets/covers/${name}`;
+
+  // TODO better handler for images.
+  let img = IndexImg;
+  if (name === "about") {
+    img = AboutImg;
+  }
 
   return (
     <section className="mb-12 shadow-lg col-span-3">
-      <Image src={path} alt="Image" layout="fill" />
+      <Image src={img} alt="Image" />
     </section>
   );
 };
