c5c9c958f2470261116d705798d7b254113e40b2
J Shamsul Bahri (jibone)) <jibone@gmail.com>
2022-02-09 12:56:59 +0800

    Revert back to using img tag

--- weblog/components/HeroImage.tsx
+++ weblog/components/HeroImage.tsx
@@ -1,19 +1,18 @@
-import Image from "next/image";
-import IndexImg from "../public/assets/covers/index.jpg";
-import AboutImg from "../public/assets/covers/about.jpg";
-
 const HeroImage: React.FC<{ name: string }> = ({ name }) => {
-  // const path = `/assets/covers/${name}`;
+  const path = `/assets/covers/${name}`;
 
   // TODO better handler for images.
-  let img = IndexImg;
+  let altText = "Random Image";
+  if (name === "index") {
+    altText = "Black and white image of a person walking in the snow";
+  }
   if (name === "about") {
-    img = AboutImg;
+    altText = "Black and white image of Macbook";
   }
 
   return (
     <section className="mb-12 shadow-lg col-span-3">
-      <Image src={img} alt="Image" />
+      <img src={path} alt={altText} />
     </section>
   );
 };
