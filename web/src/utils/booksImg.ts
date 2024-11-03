25699f570d4b0cbe9c4cbbaccc38e77ead8b521a
J Shamsul Bahri <jibone@gmail.com>
2024-11-03 14:54:13 +0800

    add more book cover for random og (#77)

--- web/src/utils/booksImg.ts
+++ web/src/utils/booksImg.ts
@@ -1,9 +1,14 @@
 const bookCoverImgs = [
-  "https://m.media-amazon.com/images/I/81pIICZO2PL._SY466_.jpg",
-  "https://m.media-amazon.com/images/I/81p4Y+0HzbL._SY466_.jpg",
-  "https://m.media-amazon.com/images/I/91OssggvIKL._SY466_.jpg",
-  "https://m.media-amazon.com/images/I/813Q+Ds32eL._SY466_.jpg",
-  "https://m.media-amazon.com/images/I/91svsZ609OL._SY466_.jpg",
+  "https://m.media-amazon.com/images/I/81pIICZO2PL._SY466_.jpg", // Neuromancer
+  "https://m.media-amazon.com/images/I/81p4Y+0HzbL._SY466_.jpg", // Snow Crash
+  "https://m.media-amazon.com/images/I/91OssggvIKL._SY466_.jpg", // Three-body Problem
+  "https://m.media-amazon.com/images/I/813Q+Ds32eL._SY466_.jpg", // Dark Forest
+  "https://m.media-amazon.com/images/I/91svsZ609OL._SY466_.jpg", // Death's end
+  "https://m.media-amazon.com/images/I/21SDL+t2wlL._SY445_SX342_.jpg", // On Bullshit
+  "https://m.media-amazon.com/images/I/91Z6ApocmwL._SY466_.jpg", // How to read a book
+  "https://m.media-amazon.com/images/I/71ZHy+J7anL._SY466_.jpg", // Ready Player One
+  "https://m.media-amazon.com/images/I/81a-GuXSbwL._SY466_.jpg", // Ready Player Two
+  "https://m.media-amazon.com/images/I/61fuQ7f70UL._SY466_.jpg", // Armada
 ];
 
 export function getBookCoverImg(count: number): string[] {
