10277b0b68f043ad3ead166c1295e8a4121283cc
Jibone <jibone@gmail.com>
2025-06-01 15:44:53 +0800

    Add brave new world and update book like

--- web/src/utils/booksImg.ts
+++ web/src/utils/booksImg.ts
@@ -5,10 +5,17 @@ const bookCoverImgs = [
   "https://m.media-amazon.com/images/I/813Q+Ds32eL._SY466_.jpg", // Dark Forest
   "https://m.media-amazon.com/images/I/91svsZ609OL._SY466_.jpg", // Death's end
   "https://m.media-amazon.com/images/I/21SDL+t2wlL._SY445_SX342_.jpg", // On Bullshit
+  "https://m.media-amazon.com/images/I/61HNRm7zq1L._SY522_.jpg", // On Truth
   "https://m.media-amazon.com/images/I/91Z6ApocmwL._SY466_.jpg", // How to read a book
   "https://m.media-amazon.com/images/I/71ZHy+J7anL._SY466_.jpg", // Ready Player One
   "https://m.media-amazon.com/images/I/81a-GuXSbwL._SY466_.jpg", // Ready Player Two
   "https://m.media-amazon.com/images/I/61fuQ7f70UL._SY466_.jpg", // Armada
+  "https://m.media-amazon.com/images/I/71GNqqXuN3L._SL1500_.jpg", // Brave New World
+  "https://m.media-amazon.com/images/I/81TGgILNUCL._SY466_.jpg", // Non-things
+  "https://m.media-amazon.com/images/I/71+7w2y6R8L._SY466_.jpg", // 1984
+  "https://m.media-amazon.com/images/I/71DCf2goJ5L._SY466_.jpg", // Animal Farm
+  "https://m.media-amazon.com/images/I/71IovFwZWlL._SY466_.jpg", // Why Write
+  "https://m.media-amazon.com/images/I/7111-QqvNCL._SY522_.jpg", // Amusing Ourselves to Death
 ];
 
 export function getBookCoverImg(count: number): string[] {
