f3273591feed0c4d453438376623c74893db24a4
J Shamsul Bahri <jibone@gmail.com>
2025-12-17 10:16:11 +0800

    Add theuth and king thamus (#123)
    * add new post theuth and king thamus
    
    * remove console.lg
    
    * fix code & codex page

--- web/src/app/code-and-codex/page.tsx
+++ web/src/app/code-and-codex/page.tsx
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import { EssayList, Layout } from "@/components";
 import { generateSiteMetadata } from "@/utils";
+import { Metadata } from "next";
 
-export async function generateMetadata() {
+export async function generateMetadata(): Promise<Metadata> {
   const title = "Code & Codex";
   const subtitle = "Essays on code, meaning, and modern myth";
   const titleEncoded = encodeURIComponent(title);
@@ -29,12 +30,10 @@ export default function CodeCodexPage() {
         </p>
 
         <p>
-          <a href="https://codeandcodex.substack.com" target="_blank">
-            Code &amp; Codex
-          </a>{" "}
-          is a newsletter I started in early 2025. It’s a cyberpunk-inspired
-          project that explores the liminal space between software engineering,
-          encrypted knowledge, and the aesthetics of digital subculture.
+          <strong>Code &amp; Codex</strong> is a newsletter I started in early
+          2025. It’s a cyberpunk-inspired project that explores the liminal
+          space between software engineering, encrypted knowledge, and the
+          aesthetics of digital subculture.
         </p>
 
         <p>
@@ -43,56 +42,43 @@ export default function CodeCodexPage() {
 
         <p>
           I consider myself a seasoned technologist with a taste for
-          storytelling.{" "}
-          <a href="https://codeandcodex.substack.com" target="_blank">
-            Code &amp; Codex
-          </a>{" "}
-          is where I weave together reflections on code, language, meaning, and
-          permanence in the ever-shifting matrix of modern tech.
+          storytelling. <strong>Code &amp; Codex</strong> is where I weave
+          together reflections on code, language, meaning, and permanence in the
+          ever-shifting matrix of modern tech.
         </p>
 
         <p>
           This newsletter isn’t a place for tutorials or trends.{" "}
-          <a href="https://codeandcodex.substack.com" target="_blank">
-            Code &amp; Codex
-          </a>{" "}
-          operates more like a digital grimoire: a dispatch of philosophical dev
-          musings, underground signals, and long-form essays that examine the
-          act of creation in code as both an engineering discipline and an
-          artistic pursuit.
+          <strong>Code &amp; Codex</strong> operates more like a digital
+          grimoire: a dispatch of philosophical dev musings, underground
+          signals, and long-form essays that examine the act of creation in code
+          as both an engineering discipline and an artistic pursuit.
         </p>
 
         <p>
           By bringing together themes like digital permanence, the search for
           truth, and the hacker-as-alchemist, I hope{" "}
-          <a href="https://codeandcodex.substack.com" target="_blank">
-            Code &amp; Codex
-          </a>{" "}
-          will resonate with readers who see technology not just as a tool, but
-          as a form of intellectual and existential expression.
+          <strong>Code &amp; Codex</strong> will resonate with readers who see
+          technology not just as a tool, but as a form of intellectual and
+          existential expression.
         </p>
 
         <p>
           Through essays, commentary, and the occasional cryptic dispatch, I
-          want <a href="https://codeandcodex.substack.com">Code &amp; Codex</a>{" "}
-          to speak to those who write code not just to build, but to create
-          meaning.
+          want <strong>Code &amp; Codex</strong> to speak to those who write
+          code not just to build, but to create meaning.
         </p>
 
         <p>
-          If any of this speaks to you, subscribe to{" "}
-          <a href="https://codeandcodex.substack.com" target="_blank">
-            Code &amp; Codex
-          </a>{" "}
-          on Substack and receive the latest dispatch straight to your inbox.
+          If any of this speaks to you, consider subscribing to Code &amp; Codex
+          on <a href="https://codeandcodex.substack.com">Substack</a>,{" "}
+          <a href="https://paragraph.com/@code_codex">Paragraph</a>, or read it
+          on{" "}
+          <a href="https://primal.net/p/nprofile1qqsqsgehv75h9hvla3d39gzh87w5karednhcaj6z3jhy5tmtsxmzgjgpwsmcx#reads">
+            Nostr
+          </a>
+          .
         </p>
-
-        <div className="space-x-4 bg-slate-50 p-2 border border-slate-200 rounded-lg mb-4">
-          <iframe
-            className="w-full"
-            src="https://codeandcodex.substack.com/embed"
-          ></iframe>
-        </div>
 
         <p>Or explore the archive here.</p>
 
